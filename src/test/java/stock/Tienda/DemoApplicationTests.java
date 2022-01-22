2df64461a38662cd9db5ee26b341688a13161187
Mirna Pamela Diaz <pamela_diaz_24@outlook.com>
2022-01-22 15:20:27 -0300

    Primer commit

---
+++ src/test/java/stock/Tienda/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package stock.Tienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
