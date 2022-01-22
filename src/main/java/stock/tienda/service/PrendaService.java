eecab822cfbe86b0a5e20bb6c1091f2efed62a4d
Mirna Pamela Diaz <pamela_diaz_24@outlook.com>
2022-01-22 18:21:36 -0300

    Se definen los metodos de PrendaService

--- src/main/java/stock/tienda/service/PrendaService.java
+++ src/main/java/stock/tienda/service/PrendaService.java
@@ -1,4 +1,18 @@
 package stock.tienda.service;
 
+import stock.tienda.model.Prenda;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface PrendaService {
+
+Prenda save(Prenda prenda);
+
+Prenda update(Long idPrenda, Prenda prenda);
+
+List<Prenda> findAll();
+
+Optional<Prenda> findById(Long idPrenda);
+
 }
