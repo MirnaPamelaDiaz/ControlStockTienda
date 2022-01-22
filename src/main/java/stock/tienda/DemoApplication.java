2c6af313ba1be55ae4fab7d64851460c15bcbfae
DESKTOP-ISTA7PU\reboo <meza.maximiliano364@gmail.com>
2022-01-22 16:21:23 -0300

    Segundo commit

--- src/main/java/stock/tienda/DemoApplication.java
+++ src/main/java/stock/tienda/DemoApplication.java
@@ -1,4 +1,4 @@
-package stock.Tienda;
+package stock.tienda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/stock/tienda/DemoApplicationTests.java
+++ src/test/java/stock/tienda/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package stock.Tienda;
+package stock.tienda;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
