ea4d16ddeb41cc2082cdb982b5cfea9be501a8ce
Mirna Pamela Diaz <pamela_diaz_24@outlook.com>
2022-01-22 16:59:46 -0300

    se construyen las entidades Prenda y Proveedor en el package model

---
+++ src/main/java/stock/tienda/model/Prenda.java
@@ -0,0 +1,31 @@
+package stock.tienda.model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+public class Prenda {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String nombre;
+    private Double precioCompra;
+    private Double precioVenta;
+    private String detalle;
+    private String temporada;
+    private Integer talle;
+    private String color;
+    //private Stock stock;
+    private Proveedor proveedor;
+    private Double porcentajeUtilidad;
+}
---
+++ src/main/java/stock/tienda/model/Proveedor.java
@@ -0,0 +1,28 @@
+package stock.tienda.model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+public class Proveedor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private  Prenda prenda;
+    private  String mail;
+    private  Long telefono;
+    private String nombre;
+    private  String direcccion;
+    private  String zona;
+}
