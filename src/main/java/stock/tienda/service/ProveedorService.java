ac5bfa2d1de135220037ab0eeffc745823a87789
Mirna Pamela Diaz <pamela_diaz_24@outlook.com>
2022-01-22 18:22:21 -0300

    Se definen los metodos de ProveedorService

--- src/main/java/stock/tienda/service/ProveedorService.java
+++ src/main/java/stock/tienda/service/ProveedorService.java
@@ -1,4 +1,18 @@
 package stock.tienda.service;
 
+import stock.tienda.model.Proveedor;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface ProveedorService {
+
+     Proveedor save(Proveedor proveedor);
+
+      Proveedor update(Long idProveedor, Proveedor proveedor);
+
+      List<Proveedor> findAll();
+
+      Optional<Proveedor> findById(Long idProveedor);
+
 }
