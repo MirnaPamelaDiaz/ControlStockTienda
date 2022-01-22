ff35ca9817f2bc8f5d112fed90b0137dd2823c2b
Mirna Pamela Diaz <pamela_diaz_24@outlook.com>
2022-01-22 18:24:42 -0300

    Se sobreescriben los metodos de ProveedorService

--- src/main/java/stock/tienda/service/ProveedorServiceImpl.java
+++ src/main/java/stock/tienda/service/ProveedorServiceImpl.java
@@ -1,7 +1,30 @@
 package stock.tienda.service;
 
 import org.springframework.stereotype.Service;
+import stock.tienda.model.Proveedor;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProveedorServiceImpl implements ProveedorService{
+    @Override
+    public Proveedor save(Proveedor proveedor) {
+        return null;
+    }
+
+    @Override
+    public Proveedor update(Long idProveedor, Proveedor proveedor) {
+        return null;
+    }
+
+    @Override
+    public List<Proveedor> findAll() {
+        return null;
+    }
+
+    @Override
+    public Optional<Proveedor> findById(Long idProveedor) {
+        return Optional.empty();
+    }
 }
